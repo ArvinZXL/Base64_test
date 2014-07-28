71d1d04f93705d2e961f950f09a07f955cc783f5
ArvinZXL <liang_arvin@163.com>
2014-07-28 17:20:08 +0800

    Test

---
+++ src/a/b/c/Test.java
@@ -0,0 +1,14 @@
+package a.b.c;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class BaseCode {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		System.out.print(a+b);
+	
+}
